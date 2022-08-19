02bff3250157993e6306c7d54ed29dc1d9311a82
Daro! <derodriguez074@misena.edu.co>
2022-08-19 17:19:28 -0500

    Javascript_Talleres

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+
+let gradosF, centigrados
+
+gradosF = prompt("Ingrese los grados en Fahrenheith");
+gradosF = parseInt(gradosF);
+
+
+centigrados = (gradosF-32)/1.8;
+
+
+//document.write(gradosF+" grados FahrenHeith son equivalentes a: "+centigrados+" grados centigrados");
+
+
+alert(`${gradosF} grados FahrenHeith son equivalentes a: ${centigrados}grados centigrados`)
+
+//Hacer un programa que simule una alcansia 
+
+//Programa que soliciite  Nombre apellido edad y telefono  y muestre  "Tu nombre es nombre  y tu apellido es tienes edad y tu telefono "
+
+
+//Programa que diga si es mayor de edad diga bienvenido y si es menor diga que es menor y si dojita una edad menor a cero decirle qu dijite una edad que corresponda
+
